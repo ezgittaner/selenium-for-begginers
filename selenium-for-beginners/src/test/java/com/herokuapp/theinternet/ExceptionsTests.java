48e892973b222e7c841b43e017daf23a3a315aef
Ezgi Taner <ezgittaner@gmail.com>
2019-04-02 14:37:52 +0300

    exceptionsTests

---
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
@@ -0,0 +1,86 @@
+package com.herokuapp.theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExceptionsTests {
+	private WebDriver driver;
+
+	@BeforeMethod
+	private void setUp() {
+		System.out.println("Creating driver");
+		// Create driver
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		driver = new ChromeDriver();
+
+		sleep(3000);
+
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void notVisibleTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebElement finishText = driver.findElement(By.id("finish"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf(finishText));
+
+		Assert.assertTrue(finishText.getText().equals("Hello World!"),
+				"'Hello World! text is not present on the page.'");
+
+	}
+
+	@Test
+	public void timeoutTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebElement finishText = driver.findElement(By.id("finish"));
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+		wait.until(ExpectedConditions.visibilityOf(finishText));
+
+		Assert.assertTrue(finishText.getText().equals("Hello World!"),
+				"'Hello World! text is not present on the page.'");
+	}
+
+	@AfterMethod
+	private void tearDown() {
+		sleep(3000);
+		System.out.println("Close driver");
+		// Close browser
+		driver.quit();
+	}
+
+	/** Static sleep */
+	private void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
--- selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,45 +1,50 @@
 package com.herokuapp.theinternet;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 public class PositiveTests {
-	
+
 	@Test
 	public void Test() {
 		System.out.println("Starting logIn test");
-		
+
 		// Create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		
+
 		sleep(3000);
-		
+
 		// driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 		// open the page
 		String url = "http://the-internet.herokuapp.com/login";
 		driver.get(url);
 		sleep(2000);
 		System.out.println("Page is opened.");
-		
+
 		// enter username
 		WebElement username = driver.findElement(By.id("username"));
 		username.sendKeys("tomsmith");
-		
+
 		// enter password
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("SuperSecretPassword!");
-		
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
 		// push log in button
 		WebElement logInButton = driver.findElement(By.className("radius"));
+		wait.until(ExpectedConditions.elementToBeClickable(logInButton));
 		logInButton.click();
-		
+
 		// new url
 		String expectedUrl = "http://the-internet.herokuapp.com/secure";
 		String actualUrl = driver.getCurrentUrl();
@@ -48,7 +53,7 @@ public class PositiveTests {
 		// log out button is visible
 		WebElement logOutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
 		Assert.assertTrue(logOutButton.isDisplayed(), "logOutButton is not visible.");
-		
+
 		// Successful log in message
 		WebElement successMessage = driver.findElement(By.id("flash"));
 		String expectedSuccessMessage = "You logged into a secure area!";
@@ -57,11 +62,12 @@ public class PositiveTests {
 				"actualSuccessMessage does not contain expectedSuccessMessage\nexpectedSuccessMessage: "
 						+ expectedSuccessMessage + "\nactualSuccessMessage: " + actualSuccessMessage);
 
-				sleep(3000);
+		sleep(3000);
 
-				// Close browser
-				driver.quit();
+		// Close browser
+		driver.quit();
 	}
+
 	/** Static sleep */
 	private void sleep(long millis) {
 		try {
