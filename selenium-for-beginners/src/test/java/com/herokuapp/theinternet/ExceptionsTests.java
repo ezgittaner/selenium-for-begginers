a506ec4e51f34dac79573463cecf153e13502eb3
Ezgi Taner <ezgittaner@gmail.com>
2019-04-02 17:37:33 +0300

    exceptionTests

--- selenium-for-beginners/src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
@@ -1,6 +1,7 @@
 package com.herokuapp.theinternet;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -59,10 +60,63 @@ public class ExceptionsTests {
 
 		WebElement finishText = driver.findElement(By.id("finish"));
 		WebDriverWait wait = new WebDriverWait(driver, 2);
-		wait.until(ExpectedConditions.visibilityOf(finishText));
+		try {
+			wait.until(ExpectedConditions.visibilityOf(finishText));
+		} catch (TimeoutException e) {
+			// Steps after exception
+			System.out.println("Exception catched:" + e.getMessage());
+			sleep(3000);
+		}
 
 		Assert.assertTrue(finishText.getText().equals("Hello World!"),
 				"'Hello World! text is not present on the page.'");
+	}
+
+	@Test
+	public void noSuchElementTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/2";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		Assert.assertTrue(
+				wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("finish"), "Hello World!")),
+				"Failed to wait for next to be present in element.");
+
+		// WebElement finishText = driver.findElement(By.id("finish"));
+		// Assert.assertTrue(finishText.getText().equals("Hello World!"),
+		// "'Hello World! text is not present on the page.'");
+
+	}
+
+	@Test
+	public void staleElementTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_controls";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement button = driver.findElement(By.xpath("//button"));
+		WebElement checkbox = driver.findElement(By.id("checkbox"));
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		button.click();
+		Assert.assertTrue(wait.until(ExpectedConditions.stalenessOf(checkbox)),
+				"Checkbox is still displayed, but it shouldn't be");
+
+		// WebElement loading = driver.findElement(By.id("loading"));
+		// wait.until(ExpectedConditions.invisibilityOf(loading));
+
+		// Assert.assertFalse(checkbox.isDisplayed(), "Checkbox is displayed, but it
+		// should not be.");
 	}
 
 	@AfterMethod
